a62fc2c7682f4378b8eb23659c9ba30ee21658cd
masoapps <developer@masoapps.com>
2015-08-04 13:35:08 +0100

    Initial Commit

---
+++ ExampleInfiniteScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleInfiniteScrollView
+//
+//  Created by Mason L'Amy on 04/08/2015.
+//  Copyright (c) 2015 Maso Apps Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
